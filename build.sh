583c8416a9d34965781c400b1dd7d69f06ed06af
Matthew Holt <mholt@users.noreply.github.com>
2026-03-10 11:54:09 -0600

    Try lowercase variable

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -ex
 
-ARTIFACT=/usr/local/bin/caddy-custom
+artifact=/usr/local/bin/caddy-custom
 
 xcaddy build \
     $(echo "$CADDY_PLUGINS" | tr ' \n' '\n' | sed '/^$/d' | sed 's/^/--with /') \
-    --output "$ARTIFACT"
+    --output "$artifact"
 
 # print info for debugging/diagnostic purposes
-"$ARTIFACT" build-info
-"$ARTIFACT" list-modules
+"$artifact" build-info
+"$artifact" list-modules
